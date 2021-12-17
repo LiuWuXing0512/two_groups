7a7f95fe1cacdfb7ac1cdb636414ed1ccbe41d27
fuyu <971113575@qq.com>
2021-12-18 07:29:24 +0800

    feat:false

---
+++ myapp/src/models/prod.tsx
@@ -0,0 +1,73 @@
+import { Record } from '@/interfaces'
+import { getProd } from '@/services';
+import { Effect, ImmerReducer, Reducer, Subscription } from 'umi';
+
+// 模块内部state接口
+export interface ProdModelState {
+    current: number,
+    size: number,
+    records:Record[]
+}
+
+// 模块的接口
+export interface ProdModelType {
+    namespace: 'prod';
+    state: ProdModelState;
+    effects: {
+        getprod: Effect;
+    };
+    reducers: {
+        save: Reducer<ProdModelState>;
+        // 启用 immer 之后
+        // save: ImmerReducer<IndexModelState>;
+    };
+
+}
+
+// 模块的定义
+const ProdModel: ProdModelType = {
+    namespace: 'prod',
+
+    state: {
+        current: 1,
+        size: 10,
+        records:[]
+    },
+
+    // 异步action
+    effects: {
+        *getprod({ payload }, { call, put, select }) {
+            // console.log(payload);
+
+            let result = yield getProd(payload);
+            // console.log(result)
+            if (result.records) {
+                yield put({
+                    type: 'save',
+                    payload: {
+                        records:result.records 
+                    }
+                })
+            }
+            // 从redux中拿到状态
+            // const state=yield select(state=>state.prod.records)
+            // console.log(state,'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee');
+            
+
+        },
+    },
+
+    // 同步action
+    reducers: {
+        save(state, action) {
+            return {
+                ...state,
+                ...action.payload,
+            };
+        },
+    },
+
+
+};
+
+export default ProdModel;
