02c4c65c08afd128f084d91ad6b4340b529bcd1c
孙sun <431884088@qq.com>
2021-12-19 19:10:57 +0800

    asdas

--- myapp/src/pages/sys/area.tsx
+++ myapp/src/pages/sys/area.tsx
@@ -1,76 +1,38 @@
-import React, { useEffect } from 'react';
-import { Input, Button, Collapse } from 'antd';
+import { Component } from 'react';
 import './area.less'
-import { CaretRightOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
-import { connect, ConnectRC } from 'umi';
-import { IaddressList, IPropsArea } from '@/interfaces';
-
-const { Panel } = Collapse;
-
-const Area: ConnectRC<IPropsArea> = (props) => {
-    const { dispatch } = props;
-    useEffect(() => {
-        dispatch({
+import { Input, Button, Tree } from 'antd';
+import { connect } from 'umi';
+import { IPropsArea } from '@/interfaces';
+const gData = [];
+class Area extends Component <any> {
+    state = {
+        gData,
+    };
+    componentDidMount() {
+        this.props.dispatch({
             type: 'area/addressList',
             payload: { t: new Date().getTime(), current: 1, size: 10 }
         })
-    }, [])
-    const { areaList } = props.area
-    return (
-        <div className='area'>
-            <div className='area-search'>
-                <Input placeholder="地区关键词" />
-                <Button type="primary">
-                    新增
-                </Button>
+    }
+    render() {
+        const { areaList } = this.props.area
+        return (
+            <div className='area'>
+                <div className='area-search'>
+                    <Input placeholder="地区关键词" />
+                    <Button type="primary">
+                        新增
+                    </Button>
+                </div>
+                <div className='area-conent'>
+                    <Tree
+                        className="draggable-tree"
+                        fieldNames={{ title: "areaName", key: "areaId" }}
+                        treeData={areaList}
+                    />
+                </div>
             </div>
-            <div className='area-conent'>
-                <Collapse
-                    bordered={false}
-                    expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
-                    className="site-collapse-custom-collapse"
-                    collapsible="header"
-                >
-                    {
-                        areaList.map(level => {
-                            return (
-                                <Panel header={level.areaName} key={level.areaId} extra={<p>
-                                    <a><EditOutlined />修改</a>&emsp;<a><DeleteOutlined />删除</a>
-                                </p>}>
-                                    {
-                                        (level.children as IaddressList[]).map(level2 => {
-                                            return (
-                                                <Collapse
-                                                    bordered={false}
-                                                    collapsible="header"
-                                                    className="site-collapse-custom-collapse level2"
-                                                    expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
-                                                >
-                                                    <Panel header={level2.areaName} key={level.areaId} extra={<p>
-                                                        <a><EditOutlined />修改</a>&emsp;<a><DeleteOutlined />删除</a>
-                                                    </p>}>
-                                                        {
-                                                            level2.children && level2.children.length > 0 ? level2.children.map(level3 => {
-                                                                return (
-                                                                    <div className='level3'>
-                                                                        <div>{level3.areaName}</div>
-                                                                        <div><a><EditOutlined />修改</a>&emsp;<a><DeleteOutlined />删除</a></div>
-                                                                    </div>
-                                                                )
-                                                            }) : null
-                                                        }
-                                                    </Panel>
-                                                </Collapse>
-                                            )
-                                        })
-                                    }
-                                </Panel>
-                            )
-                        })
-                    }
-                </Collapse>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 export default connect(({ area }: IPropsArea) => ({ area }))(Area);
